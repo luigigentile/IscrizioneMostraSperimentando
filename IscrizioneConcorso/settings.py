5b6986524d846b3e9c2aad9ce851cfe8fa8ef412
luigigentile <luigi.gentile2@gmail.com>
2022-01-22 18:43:38 +0100

    Setting

--- IscrizioneConcorso/settings.py
+++ IscrizioneConcorso/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY=os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','iscrizionemostrasperimentando.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','iscrizionesperimentando.herokuapp.com']
 
 
 # Application definition
