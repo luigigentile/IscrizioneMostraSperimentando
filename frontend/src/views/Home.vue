9fe8c59eda1c10bb5a16218b889d0a2401a25b21
luigigentile <luigi.gentile2@gmail.com>
2022-01-23 10:10:40 +0100

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -335,7 +335,7 @@ export default {
   },
     created() {
       this.getPrenotazioni();
-      this.getMovimentiPrenotazioni();
+//      this.getMovimentiPrenotazioni();
       this.getTurni();
       this.getSettori();
       this.getUserName();
