c71f5d25275b88d58c58b36b80aa1e082b4f8d7a
luigigentile <luigi.gentile2@gmail.com>
2023-02-05 10:42:25 +0100

    Aggiunto nuovo sito IscrizioneMostraSperimentando

--- IscrizioneConcorso/settings.py
+++ IscrizioneConcorso/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY=os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','iscrizionesperimentando.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','iscrizionesperimentando.herokuapp.com', 'iscrizionemostrasperimentando.herokuapp.com']
 
 
 # Application definition
